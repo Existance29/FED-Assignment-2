7ac166980e2cee9dc69da1bceae094a6b02a2869
exihs <brydonti@gmail.com>
2024-01-21 10:43:37 +0800

    minor cleanup to the scroll function
    Added comments, removed repeat code. Also use window.ScrollY for cross-browser compatibility

--- script.js
+++ script.js
@@ -18,22 +18,21 @@ function menutoggle() {
 window.onscroll=function() {scrollFunction()};
 
 function scrollFunction() {
-    if (window.innerWidth > 600) {
-        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    //account for different window sizes
+    var transitionThreshold = 500
+    //for smaller windows, trigger the transition sooner - lower threshold for transition
+    if (window.innerWidth <= 600) {
+        transitionThreshold = 150
+    }
+    //check if the user has scrolled past the threshold
+    if (window.scrollY > transitionThreshold || document.documentElement.scrollTop > transitionThreshold) {
+        //user has not scrolled past, set to white
         document.getElementById("header").style.backgroundColor = "white";
     }
     else {
-        document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)";
+        document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)"; //set to transparent
     }
-    }
-    else {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById("header").style.backgroundColor = "white";
-    }
-    else {
-    document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)";
-    }
-    }
-
 
 }
+
+/*Testing the API stuff*/
