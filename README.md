2467948383ad0407fe06a5b1164ea155529b4fea
jungsek <s10258726@connect.np.edu.sg>
2024-01-22 21:38:09 +0800

    Slideshow commit

--- README.md
+++ README.md
@@ -13,3 +13,12 @@
 
 References: 
 https://resource.logitechg.com/w_1800,h_1800,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/gaming/en/plp-hero/hero-gaming-keyboards-desktop.jpg?v=1
+
+
+
+
+Home-slideshow-2:
+https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.behance.net%2Fgallery%2F60784401%2FLogitech-Shadows&psig=AOvVaw1HkVAl4zjuP0-7fg_QGMOy&ust=1705986624189000&source=images&cd=vfe&opi=89978449&ved=0CBMQjhxqFwoTCNjBvsmd8IMDFQAAAAAdAAAAABAD
+
+Home-slideshow-3:
+https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.razer.com%2Feu-en%2Fpc%2Fgaming-laptops&psig=AOvVaw3sFLn6svb4QLEertcaqWbC&ust=1705986678588000&source=images&cd=vfe&opi=89978449&ved=0CBMQjhxqFwoTCOjg0OSd8IMDFQAAAAAdAAAAABAX
--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function scrollFunction() {
     //check if the user has scrolled past the threshold
     if (window.scrollY > transitionThreshold || document.documentElement.scrollTop > transitionThreshold) {
         //user has not scrolled past, set to white
-        document.getElementById("header").style.backgroundColor = "rgba(26,30,38,0.9)";
+        document.getElementById("header").style.backgroundColor = "rgba(26,30,38,1)";
     }
     else {
         document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)"; //set to transparent
@@ -35,27 +35,7 @@ function scrollFunction() {
 
 }
 
-
-
-/*JS FUNCTION FOR AUTOMATIC SLIDESHOW SECTION*/
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i=0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
-}
+    
 
 
 /*Testing the API stuff*/
