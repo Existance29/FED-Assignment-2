58bff0bd3cf3c5836fd8230cb9e8b4ab6e6a0a8f
exihs <brydonti@gmail.com>
2024-01-28 22:20:56 +0800

    Added lottie animation

--- gachatest.js
+++ gachatest.js
@@ -12,52 +12,56 @@ function pull(n){
     screen.style.display = "block"
     disableScroll()
     var row = document.getElementById("item-display-row")
-    var out = []
-    
-    for (var i = 0; i < n; i++){
-        var chance = Math.floor(Math.random() * 10000)*0.01
-        var push = 0
-        var rarity = ""
-        //iterate through dictionary
-        for (const [key, value] of Object.entries(dict)) {
-            //check which chance bracket the random value belongs to
-            if (chance < push+value){
-                rarity = key
-                break
-            }
-            push += value
-        }
-
-        //display the items with a border for rarity
-        if (rarity == "ultra rare"){
-            //rainbowq border for ultra rare
-            row.innerHTML += `<div style = "border: 5px solid transparent;
-                                            border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
-                                            border-image-slice: 1;">
-                                            </div>`
-        } else if (rarity == "rare"){
-            //purple border for rare
-            row.innerHTML += `<div style = "border-color:#A020F0" class = "fade-in"></div>`
-        }else{
-            //white border for common
-            row.innerHTML += `<div style = "border-color:white" class = "fade-in"></div>`
-        }
-    }
-
-    //wait 1 second before letting user continue
+    var openAnim = document.getElementById("open-anim")
+    var src = "https://lottie.host/81fe254e-8e25-44b3-a723-fcfe68b7b916/psA9kkEEVW.json"
+    openAnim.load(src)
     setTimeout(function(){
-        //show continue text
-        var text = document.getElementById("continue-text")
-        text.style.opacity = "1"
-        //add onclick event to allow user to continue
-        screen.addEventListener("click", function(){
-            screen.style.display = "none" //hide screen 
-            text.style.opacity = "0"
-            row.innerHTML = "" //clear all items display
+        for (var i = 0; i < n; i++){
+            var chance = Math.floor(Math.random() * 10000)*0.01
+            var push = 0
+            var rarity = ""
+            //iterate through dictionary
+            for (const [key, value] of Object.entries(dict)) {
+                //check which chance bracket the random value belongs to
+                if (chance < push+value){
+                    rarity = key
+                    break
+                }
+                push += value
+            }
 
-        }, { once: true })
+            //display the items with a border for rarity
+            if (rarity == "ultra rare"){
+                //rainbowq border for ultra rare
+                row.innerHTML += `<div style = "border: 5px solid transparent;
+                                                border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
+                                                border-image-slice: 1;">
+                                                </div>`
+            } else if (rarity == "rare"){
+                //purple border for rare
+                row.innerHTML += `<div style = "border-color:#A020F0" class = "fade-in"></div>`
+            }else{
+                //white border for common
+                row.innerHTML += `<div style = "border-color:white" class = "fade-in"></div>`
+            }
+        }
 
-    }, 1000);
+        //wait 1 second before letting user continue
+        setTimeout(function(){
+            openAnim.style.display = "none"
+            //show continue text
+            var text = document.getElementById("continue-text")
+            text.style.opacity = "1"
+            //add onclick event to allow user to continue
+            screen.addEventListener("click", function(){
+                text.style.opacity = "0"
+                row.innerHTML = "" //clear all items display
+                enableScroll()
+
+            }, { once: true })
+
+        }, 1000);
+    }, 2000)
 }
 
 //prevent scrolling
