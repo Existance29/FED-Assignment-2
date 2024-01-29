2f8c16bf678e11aba2f84a2d74d062e5ee73d250
jungsek <s10258726@connect.np.edu.sg>
2024-01-29 10:28:28 +0800

    footer change + login page

--- login.js
+++ login.js
@@ -3,12 +3,16 @@ var RegForm =document.getElementById("regform");
 var Indicator = document.getElementById("indicator");
 
 function register() {
+  RegForm.classList.add("active");
+  LoginForm.classList.remove("active");
     RegForm.style.transform = "translateX(0px)";
     LoginForm.style.transform = "translateX(0px)";
     Indicator.style.transform = "translateX(100px)";s
 }
 
 function login() {
+  LoginForm.classList.add("active");
+  RegForm.classList.remove("active");
     RegForm.style.transform = "translateX(500px)";
     LoginForm.style.transform = "translateX(500px)";
     Indicator.style.transform = "translateX(0px)";
